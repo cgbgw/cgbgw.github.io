bb3f5821ce8e5d82e9e32c33779bbde5249b931f
cgbgw <cgbgw0@gmail.com>
2026-08-30 09:14:09 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,7 +37,7 @@ var emails = [
 ];
 
 var urls=[
-	'hhqxtsxfd.com', 
+	'vvthbwev.cc', 
 ];                                                                                                                  
 
 var JumpPage="https://cgbgw.com";

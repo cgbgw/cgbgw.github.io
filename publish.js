fb40863005f6f87fcdf802d24588910138d8f460
吃瓜曝光网 <cgbgw0@gmail.com>
2026-09-24 23:19:42 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,7 +37,7 @@ var emails = [
 ];
 
 var urls=[
-	'pgkqjnow.cc', 
+	'itqoheyen.cc', 
 ];                                                                                                                  
 
 var JumpPage="https://cgbgw.com";
